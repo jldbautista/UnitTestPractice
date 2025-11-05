c0b1d9f09b57bacef6e4d212195ec8ee91f473c4
Joseph Bautista <jdbautista1@csuchico.edu>
2025-11-05 11:44:27 -0800

    Added 5 Other Test Cases

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -17,6 +17,35 @@ class PasswordTest : public ::testing::Test
 TEST(PasswordTest, single_letter_password)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(1, my_password.count_leading_characters("Z"));
+}
+TEST(PasswordTest, two_leading_letters)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("aab"));
+}
+TEST(PasswordTest, same_long_string)
+{
+	Password my_password;
+	ASSERT_EQ(9, my_password.count_leading_characters("aaaaaaaaa"));
+}
+TEST(PasswordTest, capital_letters)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("ZzZzzWwW"));
+}
+TEST(PasswordTest, special_characters)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("$$ex$$"));
+}
+TEST(PasswordTest, empty_string)
+{
+	Password my_password;
+	ASSERT_EQ(0, my_password.count_leading_characters(""));
+}
+TEST(PasswordTest, upper_case_letters)
+{
+	Password my_password;
+	ASSERT_EQ(3, my_password.count_leading_characters("ZZZ"));
 }
